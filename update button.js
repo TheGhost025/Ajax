17e6d11cb89a09f160e9d13a92dfb12ab108d159
TheGhost025 <mazenkaluoby111@gmail.com>
2022-05-16 14:44:37 +0200

    update

--- update button.js
+++ update button.js
@@ -1,20 +1,9 @@
 document.getElementById("button").onclick = function() {
   var xhtml= new XMLHttpRequest();
-  let datatab = [
-      name= nameInput.value(),
-      emai =emailInput.value(),
-      mobile_number = mobileInput.value(),
-      birth = dateInput.value(),
-      gpa = gpaInput.value(),
-      gender = data.gender,
-      level = data.level,
-      status = data.status,
-      department = data.department, 
-  ]
 xhtml.open("POST","manage.py");
 xhtml.onreadystatechange =function() {
   if(this.readyState==4 && this.status==200){
-      xhtml.send(data);
   }
 };
+xhtml.send(data);
 }
